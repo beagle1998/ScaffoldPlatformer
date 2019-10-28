c60f342c0142ad6ec51128ce579aa07da211f2af
Mignon April Lee <leeapril137@gmail.com>
2019-10-27 17:58:02 -0700

    Platforms

---
+++ New Unity Project/Assets/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //void Awake()
+    //{
+    //    Debug.Log("Awake"); //This prints "Awake"
+    //}
+    // Use this for initialization
+    //public string my_string;
+    GameObject sprite;
+    SpriteRenderer sprite_renderer;
+    //string my_str = "Hello World";
+    void Start()
+    {
+        //Debug.Log("Start!"); //This prints "Start!"
+        //Debug.Log(my_string);
+        //find the sprite
+        sprite = GameObject.Find("My Sprite");
+        //get the sprute's spriterenderer componenr
+        sprite_renderer = sprite.GetComponent<SpriteRenderer>();
+        sprite_renderer.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("UPDATE!"); //This prints "UPDATE!"
+    }
+}
---
+++ New Unity Project/Assets/trial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trial : MonoBehaviour
+{
+    GameObject player;
+    SpriteRenderer player_sprite;
+    string playerName;
+    void Awake()
+    {
+        playerName = "Player";
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the Player
+        player = GameObject.Find(playerName);
+        //Get the player's SpriteRenderer component
+        player_sprite = player.GetComponent<SpriteRenderer>();
+        Debug.Log("hmmm");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set the player's sprite color to a random value each frame
+        player_sprite.color = new Color(
+            Random.Range(0f, 1f), //Red
+            Random.Range(0f, 1f), //Green
+            Random.Range(0f, 1f), //Blue
+            1f); //Alpha
+    }
+}
