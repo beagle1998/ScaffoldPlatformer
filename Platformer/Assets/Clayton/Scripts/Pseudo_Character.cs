d62a4eb9a2e01418ccffb812fcf5d42663210257
Clayton <claytontamtc@gmail.com>
2019-11-23 18:10:17 -0800

    We made a jumping snail

---
+++ Platformer/Assets/Clayton/Scripts/Pseudo_Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pseudo_Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Transform Ground_Check;
+    public GameObject player1;
+    public GameObject snail;
+    private float old_y_position;
+
+    void Start()
+    {
+        old_y_position = snail.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        if (player1.transform.position.y <= (snail.transform.position.y + 10f) & player1.transform.position.y >= (snail.transform.position.y - .25f))
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, player1.transform.position.y);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, old_y_position);
+        }
+        
+    }
+}
